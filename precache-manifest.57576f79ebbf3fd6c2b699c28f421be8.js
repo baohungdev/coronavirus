605e65675789a058c7b98cb639cd537dc27b376c
Dương Bảo Hưng <56499659+motcondicontrinh1@users.noreply.github.com>
2020-04-26 20:34:06 +0700

    Updates

---
+++ precache-manifest.57576f79ebbf3fd6c2b699c28f421be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d976ed0131521968ad4ecfef4f8b03bd",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "a55c968e47a3855c2d16",
+    "url": "/coronavirus/static/css/main.b4f35466.chunk.css"
+  },
+  {
+    "revision": "9052a1768dee4c47651e",
+    "url": "/coronavirus/static/js/2.343480f2.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/coronavirus/static/js/2.343480f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55c968e47a3855c2d16",
+    "url": "/coronavirus/static/js/main.0e50f1ef.chunk.js"
+  },
+  {
+    "revision": "f8ceef36d3301a297fba",
+    "url": "/coronavirus/static/js/runtime-main.9c993bcf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus/static/media/image.d7265326.png"
+  }
+]);
